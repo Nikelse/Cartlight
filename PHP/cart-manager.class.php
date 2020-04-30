2b41efc5e9e2d5ac03a2bc05365b74ae57db9c78
Nikelse <felnik@outlook.fr>
2020-04-30 22:00:48 +0200

    update 1.1

--- PHP/cart-manager.class.php
+++ PHP/cart-manager.class.php
@@ -19,6 +19,7 @@ public function ResetCart () {
 /* export id and quantity into csv file for further implementation */
 public function ExportCartData () {
   $csv_data = null;
+  /* formatting csv datas */
   foreach ($_SESSION['CART'] as $ROW) {
     $csv_data .= strval($ROW['id-product']) . ";" . strval($ROW['qty-product']) . "\r\n";
     }
